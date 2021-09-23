3e02e4bc9a609f02ba554e65537da34baed26a7d
julienshim <25723428+julienshim@users.noreply.github.com>
2021-09-22 19:56:08 -0700

    simplify

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,49 +1,42 @@
 import React from "react";
 import "./Nav.css";
 
-const Nav = props => {
+const Nav = (props) => {
+  const messageEl = document.getElementById("message");
 
-    const messageClass = () => {
-        const messageEl = document.getElementById('message');
-        if (props.message === "You guessed correctly!") {
-            if(messageEl.classList.contains('correct')) {
-              messageEl.classList.remove('correct');
-            }
-            if(messageEl.classList.contains('incorrect')) {
-              messageEl.classList.remove('incorrect');
-            }
-            setTimeout(function(){
-              messageEl.classList.add('correct')
-            }, 50);
-        } else if (props.message === "You guessed incorrectly!") {
-          if(messageEl.classList.contains('correct')) {
-            messageEl.classList.remove('correct');
-          }
-          if(messageEl.classList.contains('incorrect')) {
-            messageEl.classList.remove('incorrect');
-          }
-          setTimeout(function(){
-            messageEl.classList.add('incorrect')
-          }, 50);
-        }
+  function setMessageClass(newClass) {
+    if (messageEl.classList.contains("correct")) {
+      messageEl.classList.remove("correct");
     }
+    if (messageEl.classList.contains("incorrect")) {
+      messageEl.classList.remove("incorrect");
+    }
+    setTimeout(function () {
+      messageEl.classList.add(newClass);
+    }, 50);
+  }
 
-    return (
-        <nav className="navbar">
-            <ul>
-                <li className="brand">
-                    {props.brand}
-                </li>
-                <li id="message" className={messageClass()}>
-                    {props.message}
-                </li>
-                <li>
-                    <span className="score"> Score: {props.score}</span> <span className="scoreSplit">|</span> <span className="score">Top Score: {props.hiScore}</span>
-                </li>
-            </ul>
-        </nav>
-    )
+  if (props.message === "You guessed correctly!") {
+    setMessageClass("correct");
+  }
 
-}
+  if (props.message === "You guessed incorrectly!") {
+    setMessageClass("incorrect");
+  }
+
+  return (
+    <nav className="navbar">
+      <ul>
+        <li className="brand">{props.brand}</li>
+        <li id="message">{props.message}</li>
+        <li>
+          <span className="score"> Score: {props.score}</span>{" "}
+          <span className="scoreSplit">|</span>{" "}
+          <span className="score">Top Score: {props.hiScore}</span>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Nav;
